eab3a8a1a2bfc824091ecf1af94466d19c2324e0
Varun Dey <varun.silversurfer@gmail.com>
2015-12-20 04:35:06 +0530

    Added details like yellow card and goals

--- notify.py
+++ notify.py
@@ -2,15 +2,16 @@ import time
 import pynotify
 import requests
 from bs4 import BeautifulSoup as b
-#####
+######### club variables
 la_liga= "http://www.livescore.com/soccer/spain/primera-division/"
 bpl = "http://www.livescore.com/soccer/england/premier-league/"
 bundesliga="http://www.livescore.com/soccer/germany/bundesliga/"
-
+########notification refresh interval in seconds####
 seconds = 60
-
-url= la_liga
-
+########change the url below to whatever league you want to follow###
+url = "http://www.livescore.com/soccer/bolivia/primera-division-apertura/"
+######change the icon as well
+icon  = "/home/varun/live-football-update/images/"+ "bundesliga" +".png"
 
 soup=b(requests.get(url).content,"lxml")
 
@@ -18,6 +19,37 @@ current_match = soup.findAll("div",{"class":"row-gray even"})[0]
 
 pynotify.init ("icon-summary-body")
 
+###############################################
+def getdetails(content):
+	count=0
+	while count != len(url):	
+		for i in content:
+			a=[]
+			if "match details" in i.text:
+				continue
+			if "venue" in i.text:
+				break
+
+			else:
+				event_time = i.findAll("div")[0].text
+				a.append(event_time)
+
+			players =  i.findAll("span",{"class":"name"})
+			for x in players:
+				if x.text.strip():
+					a.append(x.text.strip())
+					
+					if i.findAll("span",{"class":"inc redcard"}):
+						a.append("Red Card")
+					
+					if i.findAll("span",{"class":"inc yellowcard"}):
+						a.append("Yellow Card")
+
+					if i.findAll("span",{"class":"inc goal"}):
+						a.append("Goal")
+		count+=1
+	return a
+######################################################
 if current_match.find("img"):
 
 	match_time = current_match.findAll("div")[0].text
@@ -27,14 +59,17 @@ if current_match.find("img"):
 		home_team = current_match.findAll("div")[1].text
 
 		score = current_match.findAll("div")[2]
-
+		################################################################################
 		details ="http://www.livescore.com/"+ score.find('a').get('href')
 
 		details_soup = b(requests.get(details).content,"lxml")
-		
+
+		body = details_soup.findAll("div",{"data-id":"details"})
+		a=getdetails(body)
+		notification = pynotify.Notification(a[0],a[1]+" "+ a[2])
+		notification.show()
+		#############################################################################
 		away_team = current_match.findAll("div")[3].text
-		 
-		icon  = "/home/varun/live-football-update/images/la_liga.png"
 
 		notification = pynotify.Notification(	home_team + ' vs ' + away_team, score.text + " in "+ match_time , icon	)
 		notification.show()
@@ -42,5 +77,5 @@ if current_match.find("img"):
 		time.sleep(seconds)
 
 else:
-	notification = pynotify.Notification(	 "No live match going on",icon	)
+	notification = pynotify.Notification(	 "No live match going on",'', icon	)
 	notification.show()
---
+++ test.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup as b
+url = 'http://www.livescore.com/soccer/spain/primera-division/valencia-vs-getafe/1-2030235/'
+soup = b(requests.get(url).content,"lxml")
+
+content = soup.findAll("div",{"data-id":"details"})
+
+count = 0
+
+while count!=len(content):
+	for i in content:
+		a=[]
+		if "match details" in i.text:
+			continue
+		if "venue" in i.text:
+			break
+
+		else:
+			event_time = i.findAll("div")[0].text
+			a.append(event_time)
+
+		players =  i.findAll("span",{"class":"name"})
+		for x in players:
+			if x.text.strip():
+				a.append(x.text.strip())
+				
+				if i.findAll("span",{"class":"inc redcard"}):
+					a.append("Red Card")
+				
+				if i.findAll("span",{"class":"inc yellowcard"}):
+					a.append("Yellow Card")
+
+				if i.findAll("span",{"class":"inc goal"}):
+					a.append("Goal")
+		print a
+	count+=1
